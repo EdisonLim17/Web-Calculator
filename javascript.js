89a2dd0c1ce41570bb13b8280e3670ca7a25f5c9
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-12-15 19:43:08 -0500

    Added functionality to display, allowing only valid inputs to be displayed. The equals operation has not been implemented yet.

--- javascript.js
+++ javascript.js
@@ -33,4 +33,84 @@ function operate(num1, op, num2){
     }
 }
 
+function addValToDisplay(displayVal, val){
+    let lastChar = displayVal[displayVal.length - 1];
+    if(val === "AC"){
+        displayVal = "";
+    }
+    else if(val === "C"){
+        displayVal = displayVal.slice(0, displayVal.length - 1);
+    }
+    else if(val === "%"){
+        if(lastChar === "÷" || lastChar === "×" || lastChar == "-" || lastChar === "+"){
+            displayVal = displayVal.slice(0, displayVal.length - 1).concat(val);
+        }
+        else {
+            displayVal = displayVal.concat(val);
+        }
+    }
+    else if(val === "÷" || val === "×" || val === "+"){
+        if(lastChar === "-"){
+            let secondLastChar = displayVal[displayVal.length - 2];
+            if(secondLastChar === "÷" || secondLastChar === "×"){
+                displayVal = displayVal.slice(0, displayVal.length - 2).concat(val);
+            }
+            else{
+                displayVal = displayVal.slice(0, displayVal.length - 1).concat(val);
+            }
+        }
+        else if(lastChar === "÷" || lastChar === "×" || lastChar === "+"){
+            displayVal = displayVal.slice(0, displayVal.length - 1).concat(val);
+        }
+        else{
+            displayVal = displayVal.concat(val);
+        }
+    }
+    else if(val === "-"){
+        if(lastChar === "+"){
+            displayVal = displayVal.slice(0, displayVal.length - 1).concat(val);
+        }
+        else if(!(lastChar === "-")){
+            displayVal = displayVal.concat(val);
+        }
+    }
+    else if(val === "."){
+        if(lastChar === "÷" || lastChar === "×" || lastChar == "-" || lastChar === "+"){
+            displayVal = displayVal.concat("0.");
+        }
+        else if(!isNaN(lastChar)){
+            let lastNum = findLastNum(displayVal);
+            if(!lastNum.includes(".")){
+                displayVal = displayVal.concat(val);
+            }
+        }
+    }
+    else{
+        displayVal = displayVal.concat(val);
+    }
+    
+    return displayVal;
+}
+
+function findLastNum(str){
+    let s = "";
+    for(let i = str.length - 1; i >= 0; i--){
+        if(!isNaN(str[i]) || str[i] === "."){
+            s = s.concat(str[i]);
+        }
+        else{break;}
+    }
+    return s;
+}
+
 let num1, num2, op;
+let displayVal = "";
+
+let display = document.querySelector("#display");
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        displayVal = addValToDisplay(displayVal, button.textContent);
+        display.textContent = displayVal;
+    });
+});
